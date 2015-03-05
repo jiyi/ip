58867535b5ef9773697a6a817789d8fd79609e35
Jiyi <jiyixx@gmail.com>
2015-03-05 16:41:12 +0800

    列表初始化

---
+++ qiuliang.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+
+  <body>
+
+    <h1>IP 数据维护功能表</h1>
+
+    <form>
+      <table>
+        
+        <tbody><tr>
+          <td>操作时间：</td>
+          <td><input /></td>
+          <td>制作人员：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>业务类型：</td>
+          <td>
+            <select>
+              <option>option</option>
+            </select>
+          </td>
+        </tr>
+        
+        <tr>
+          <td>客户名称：</td>
+          <td><input></td>
+          <td>CRM速率：</td>
+          <td><input></td>
+          <td>开通速率：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>业务接入号：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>出局设备：</td>
+          <td><input></td>
+          <td>管理IP：</td>
+          <td><input></td>
+          <td>端口：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>ONU MAC地址：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>网关设备</td>
+          <td>
+            <select>
+              <option>option</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>用户IP：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>子网掩码：</td>
+          <td>
+            <select>
+              <option>option</option>
+            </select>
+          </td>
+        </tr>
+        
+        <tr>
+          <td>网关IP：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>内层VLAN：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>外层VLAN：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>逻辑接口：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>联系人：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>联系电话：</td>
+          <td><input></td>
+        </tr>
+        
+        <tr>
+          <td>证件号码：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td>装机地址：</td>
+          <td><input></td>
+        </tr>
+
+        <tr>
+          <td><input value="生成文本：" type="button"></td>
+          <td colspan="5">
+            <textarea></textarea>
+          </td>
+        </tr>
+        
+        <tr>
+          <td>配置脚本：</td>
+          <td colspan="5">
+            <textarea></textarea>
+          </td>
+        </tr>
+
+        <tr>
+          <td>备注：</td>
+          <td colspan="5">
+            <textarea></textarea>
+          </td>
+        </tr>
+
+        <tr><td></td>
+          <td><input value="提交" type="submit"></td>
+          
+        </tr></tbody></table>
+    </form>
+
+  </body>
+</html>
